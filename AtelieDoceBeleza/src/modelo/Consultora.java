82be6c1018909e0f6ee3eaa17d543caa7d98530d
William Rodrigues <ra99514@uem.br>
2018-04-27 10:14:00 -0300

    #Implementação Consultora (William Rodrigues) +Implementação da classe Consultora no pacote modelo

--- AtelieDoceBeleza/src/modelo/Consultora.java
+++ AtelieDoceBeleza/src/modelo/Consultora.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
 
 /**
  *
- * @author Professional
+ * @author William Rodrigues da Silva
  */
-public class Consultora {
+public class Consultora implements Serializable{
+    
+    //ATRIBUTOS
+    private Integer codigo;
+    private String nome;
+    private Integer cpf;
+    private Integer codSite;
+    private Calendar dataNascimento;
+
+    //CONSTRUTOR
+    public Consultora() {
+    }
+
+    //METODOS
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getCodSite() {
+        return codSite;
+    }
+
+    public void setCodSite(Integer codSite) {
+        this.codSite = codSite;
+    }
+
+    public Calendar getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Calendar dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Consultora other = (Consultora) obj;
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        return true;
+    }    
+    
     
 }
