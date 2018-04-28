a764a7a97b713ff6a78ea6e79238289e0f7bac7b
William Rodrigues <ra99514@uem.br>
2018-04-28 12:52:06 -0300

    #Implementação DAOConsultora e Dados (William Rodrigues) +Implementação da Classe DAOConsultora no pacote modelo +Inserção da Classe Dados para manipulação dos dados da aplicação

---
+++ AtelieDoceBeleza/src/modelo/DAOConsultora.java
@@ -0,0 +1,28 @@
+
+package modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author William Rodrigues da Silva
+ */
+public class DAOConsultora {
+    
+    //Retorna lista de Consultoras (Futura persistência com Banco de Dados)
+    public List<Consultora> getLista(){
+        return Dados.listaConsultoras;
+    }
+    
+    //Insere uma nova Consultora (Futura persistência com Banco de Dados)
+    public boolean salvar(Consultora consultora){
+        if(consultora.getCodigo()==null){
+            //Se é um novo objeto, gerar código para ele.
+            Integer codigo = Dados.listaConsultoras.size() + 1;
+            consultora.setCodigo(codigo);
+            Dados.listaConsultoras.add(consultora);
+        }
+        return true;
+    }
+    
+}
---
+++ AtelieDoceBeleza/src/modelo/Dados.java
@@ -0,0 +1,17 @@
+
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author William Rodrigues da Silva
+ * Classe para manipulação dos dados,
+ * contendo apenas atributos públicos.
+ */
+public class Dados {
+        
+    public static List<Consultora> listaConsultoras = new ArrayList<>();
+    //public static List<Produto> listaProdutos = new ArrayList<>();
+}
