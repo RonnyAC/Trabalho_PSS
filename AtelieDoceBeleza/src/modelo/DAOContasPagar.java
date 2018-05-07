26f01568acb7a91409f740eceb362131e8288d9b
Ronny <ronny.proxy@gmail.com>
2018-05-07 18:53:17 -0300

    mais alterações banco

--- AtelieDoceBeleza/src/modelo/DAOContasPagar.java
+++ AtelieDoceBeleza/src/modelo/DAOContasPagar.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,7 +27,7 @@ public class DAOContasPagar {
 
     public boolean salvar(ContasPagar contasPagar) {
         int newID = buscarID();
-        
+
         ConectaSQLite conectaSQLite = new ConectaSQLite();
         conectaSQLite.conectar();
 
@@ -44,7 +45,7 @@ public class DAOContasPagar {
 
         PreparedStatement preparedStatement = conectaSQLite.criarPreparedStatement(sqlInsert);
 
-        try {                           
+        try {
             preparedStatement.setInt(1, newID);
             preparedStatement.setString(2, contasPagar.getDescricaoConta());
             preparedStatement.setString(3, contasPagar.getDataConta());
@@ -54,7 +55,7 @@ public class DAOContasPagar {
             preparedStatement.setDouble(7, contasPagar.getValorParcela());
             preparedStatement.setString(8, contasPagar.getContaPaga());
 
-            int resultado = preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
 
@@ -71,9 +72,48 @@ public class DAOContasPagar {
         return true;
     }
 
-    public boolean alterar(int possicao, ContasPagar conta) {
-        Dados.listaContasPagar.set(possicao, conta);
-        conta.codigo(possicao + 1);
+    public boolean alterar(int possicao, ContasPagar contasPagar) {
+        ConectaSQLite conectaSQLite = new ConectaSQLite();
+        conectaSQLite.conectar();
+
+        PreparedStatement preparedStatement = null;
+
+        String sql = "UPDATE tbl_contas_pagar"
+                + " SET "
+                + " descricao = ?,"
+                + " data_conta = ?,"
+                + " parcelas = ?,"
+                + " data_vencimento = ?,"
+                + " valor_total = ?,"
+                + " valor_parcela = ?,"
+                + " conta_paga = ?"
+                + " WHERE id = ?";
+
+        try {
+            preparedStatement = conectaSQLite.criarPreparedStatement(sql);
+
+            preparedStatement.setString(1, contasPagar.getDescricaoConta());
+            preparedStatement.setString(2, contasPagar.getDataConta());
+            preparedStatement.setInt(3, contasPagar.getParcelas());
+            preparedStatement.setString(4, contasPagar.getDataVencimento());
+            preparedStatement.setDouble(5, contasPagar.getValorTotal());
+            preparedStatement.setDouble(6, contasPagar.getValorParcela());
+            preparedStatement.setString(7, contasPagar.getContaPaga());
+            preparedStatement.setInt(8, contasPagar.codigo());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+                conectaSQLite.desconectar();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         return true;
     }
 
@@ -83,17 +123,17 @@ public class DAOContasPagar {
         conectaSQLite.conectar();
         ResultSet resultSet = null;
         Statement statement = null;
-        
+
         String query = "SELECT coalesce(MAX(id), 0)+1 as newID FROM tbl_contas_pagar";
-        
+
         statement = conectaSQLite.criarStatement();
-        
+
         try {
             resultSet = statement.executeQuery(query);
             newID = resultSet.getInt("id");
         } catch (SQLException e) {
             System.err.println("ID nao encontrado");
-        }finally{
+        } finally {
             try {
                 resultSet.close();
                 statement.close();
@@ -102,7 +142,51 @@ public class DAOContasPagar {
                 System.err.println("Erro no fechamento");
             }
         }
-        
+
         return newID;
+    }
+
+    public ArrayList<ContasPagar> buscar() {
+        ConectaSQLite conectaSQLite = new ConectaSQLite();
+        conectaSQLite.conectar();
+        ResultSet resultSet = null;
+        Statement statement = null;
+        ArrayList<ContasPagar> contas = new ArrayList<>();
+
+        String query = "SELECT * FROM tbl_contas_pagar";
+
+        statement = conectaSQLite.criarStatement();
+
+        try {
+            resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                ContasPagar conta = new ContasPagar();
+                
+                conta.codigo(resultSet.getInt("id"));
+                conta.setDescricaoConta(resultSet.getString("descricao"));
+                conta.setDataConta(resultSet.getString("data_conta"));
+                conta.setParcelas(resultSet.getInt("parcelas"));
+                conta.setDataVencimento(resultSet.getString("data_vencimento"));
+                conta.setValorTotal((float)resultSet.getDouble("valor_total"));
+                conta.setValorParcela((float)resultSet.getDouble("valor_parcela"));
+                //conta.setContaPaga(resultSet.getBoolean("conta_paga"));
+                
+                contas.add(conta);
+
+            }
+        } catch (SQLException e) {
+            System.err.println("ID nao encontrado");
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                conectaSQLite.desconectar();
+            } catch (SQLException e) {
+                System.err.println("Erro no fechamento");
+            }
+        }
+
+        return contas;
     }
 }
